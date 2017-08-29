f943a7ece9c3aca059ada8f968729b9b8a428209
Akhmad Faris Faizaki <faris.faizaki@gmail.com>
2017-08-29 09:04:16 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array){
+  for(let counter=0;counter<25;counter++){
+    if(counter===1){
+      array.push("I am 1 strange loop.");
+    }else{
+      array.push(`I am ${counter} strange loops.`);
+    }
+  }
+  return array;
+}
+
+function whileLoop(counter){
+  while(counter>0){
+    console.log(counter);
+    counter--;
+  }
+  return "done";
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
+function doWhileLoop(array){
+  do{
+    array.pop();
+  }while(array.length>0 && this.maybeTrue());
+}
