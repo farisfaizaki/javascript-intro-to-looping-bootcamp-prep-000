4f5ba139bf4f4c3dbdd17f052b02bfe5f0c60329
Akhmad Faris Faizaki <faris.faizaki@gmail.com>
2017-08-29 09:08:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,6 +23,7 @@ function maybeTrue() {
 
 function doWhileLoop(array){
   do{
-    array.shift();
-  }while(array.length>0 && this.maybeTrue());
+    array.pop();
+    return array;
+  }while(array.length>0 && maybeTrue());
 }
